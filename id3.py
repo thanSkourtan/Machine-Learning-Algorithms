29e7ceeaf57b9e5ad916c14a070457af67e84a14
Than Skourtan <thanosnph@gmail.com>
2015-11-26 18:43:43 +0200

    Added and tested entropy function. Started implementing id3 algorithm.

--- id3.py
+++ id3.py
@@ -1,12 +1,13 @@
 """
 Created on 24 Nov 2015
 
-Implements the ID3 algorithm as described here https://en.wikipedia.org/wiki/ID3_algorithm
+Implements the ID3 algorithm as described in "Machine Learning", Tom Mitchell, Chapter 3
 
 @author: thanSkourtan
 """
 
 import itertools 
+from math import log
 
 class Node:
     
@@ -27,17 +28,36 @@ class Node:
     
     """
     
-    def __init__(self,attribute=None,value=None,left_child=None,right_sibling=None,parent=None):
+    def __init__(self,label,attribute=None,value=None,left_child=None,right_sibling=None,parent=None):
+        self.label = label
         self.attribute = attribute
         self.value = value
         self.left_child = left_child
         self.right_sibling = right_sibling
         self.parent = parent
         
+        
 
-#def id3(data):
- #   if()
-  #  Node()
+def id3(data,target_attribute,attributes):
+    
+    values_distribution = values_per_column(data,len(data[0])-1)
+    keys = list(values_distribution.keys())
+    
+    
+    if len(keys) == 1 and keys[0] == "positive\\n":
+        return Node("positive")
+    elif len(keys) == 1 and keys[0] == "negative\\n":
+        return Node("negative")
+    elif len(keys) == 0:
+        return Node()
+    
+    
+    
+#    for attribute in attributes:
+        
+    
+    
+    #Node()
     
 def values_per_column(data,column):
     """
@@ -55,10 +75,24 @@ def values_per_column(data,column):
     return values_distribution
           
 
+def calculate_entropy(data,column):
+    entropy = 0.0
+    values_distribution = values_per_column(data,column)
+    for separate_value in values_distribution.values():
+        entropy -= separate_value/len(data)* log(separate_value/len(data),2)
+    return entropy
+
+
+
+
+
 
 file = [line.split(",") for line in itertools.islice(open("..//Python//heart_train.arff.txt"),16,None)]
 
-lala = values_per_column(file,13)
+#lala = values_per_column(file,13)
 
-print(file)
+#lala = calculate_entropy(my_data,4)
+
+
+#print(lala)
 
