233ada28570123931737a82d8a0199373d3c7f20
Than Skourtan <thanskourtan@gmail.com>
2016-04-07 08:35:51 +0300

    Implemented Prim's algorithm. Thorough testing pending.

--- clustering/graph_theory.py
+++ clustering/graph_theory.py
@@ -2,23 +2,37 @@
 @author: than_skourtan
 """
 
+from heapq import heappush, heappop
 import numpy as np
-from clustering.distance_metrics import euclidean_distance
+from distance_metrics import euclidean_distance
+from sys import maxsize as max_integer
+from data_instance import DataInstance
 
-class DataInstance:
+"""
+
+References: CLRS for MST algorithm
+"""
+class Node(DataInstance):
     
-    def __init__(self, feature_vector):
-        self.feature_vector = feature_vector #should be numpy array
-        
-        
+    """DataInstance objects also work as the nodes of the graph with regards
+       to the MST algorithm. 
+    """
+    def __init__(self, feature_vector, id):
+        DataInstance.__init__(self, feature_vector)
+        self.id = id
+        self.min_edge_weight = max_integer
+        self.parent = None
     
+    def __lt__(self, other):
+        return self.min_edge_weight < other.min_edge_weight
+                
     
 def mst(data):
-    __construct_complete_graph(data)
+    construct_complete_graph(data)
     pass
     
     
-def __construct_complete_graph(data):
+def construct_complete_graph(data):
     """Gets a list of data_instances and returns a graph represented by a two dimensional numpy array"""
     #data is a list of data_instances
     graph = np.zeros((len(data),len(data)))
@@ -27,7 +41,23 @@ def __construct_complete_graph(data):
             graph[i][j] = graph[j][i] = euclidean_distance(data[i], data[j])
     return graph
 
-
+def prim_mst(data, graph):
+    
+    data[0].min_edge_weight = 0
+    
+    heap = []
+    for node in data:
+        heappush(heap, node)
+        
+    while heap:        
+        current_node = heappop(heap)
+               
+        for node in heap: #all nodes in heap are adjacent nodes, this would be wrong otherwise
+            if graph[current_node.id][node.id] < node.min_edge_weight:
+                    node.min_edge_weight = graph[current_node.id][node.id]
+                    node.parent = current_node.id
+    return data
+    
 
 
 
--- clustering/tests/test_MST.py
+++ clustering/tests/test_MST.py
@@ -4,28 +4,23 @@
 
 import unittest
 from random import randint
-from graph_theory import DataInstance, __construct_complete_graph
+from clustering.graph_theory import *
 from utility import diagrams
 import numpy as np
 
 class MST(unittest.TestCase):
     
-    
-    
     def setUp(self):
-        self.data = [DataInstance(np.array([randint(0,100), randint(0,100)])) for i in range(0,10)]   
-        d = diagrams.Diagram()
-        d.scatter_plot(self.data)
+        self.data = [Node(np.array([randint(0,100), randint(0,100)]), i) for i in range(0,5)]   
+        #d = diagrams.Diagram()
+        #d.scatter_plot(self.data)
         
-        
+    
     def test_construct_complete_graph(self):
-        pass
+        MST.graph = construct_complete_graph(self.data)
         
-    
-    def test_MST(self):
-        __construct_complete_graph(self.data)
-    
-    
+    def test_prim_mst(self):
+        prim_mst(self.data, MST.graph)
     
     
 if __name__ == "__main__":
